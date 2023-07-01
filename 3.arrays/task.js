c088858a1742293c502332d4b50ebb40c7a89433
YuryKozachek <show_@mail.ru>
2023-07-01 11:15:45 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if(arr1.length === arr2.length){
+    return arr1.every((a, i) => a === arr2[i]);
+  }else{
+    return false;
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filterUsers = users.filter(i => i.gender === gender);
+  if(filterUsers.length === 0){
+    return 0;
+  }
+  const reduceUsers = filterUsers.reduce((acc, i) => acc + i.age, 0);
+  const resultAge = reduceUsers / filterUsers.length;
+  return resultAge;
 }
