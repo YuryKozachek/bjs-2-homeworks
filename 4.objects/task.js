a418fe9e517b9d0ee296a3c743c9acdce8075f0d
YuryKozachek <show_@mail.ru>
2023-07-06 23:17:18 +0300

    objects task fix

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,7 +2,7 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-  const marks = [];
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
