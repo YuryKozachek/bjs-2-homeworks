effae5c4da25e642605e8a7128a345f97460d8eb
YuryKozachek <show_@mail.ru>
2023-07-06 23:07:56 +0300

    objects task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  const marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+ 
+  this.marks = marksToAdd;
+   if(!this.marks){
+   console.log("Студент отчислен");
+   return;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.marks || !this.marks.length){
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, i) => acc + i);
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
 }
