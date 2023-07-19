3fdaa10bac88590b1c33fa644ddd76def1161155
YuryKozachek <show_@mail.ru>
2023-07-20 00:20:52 +0300

    exception task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,43 @@
-﻿
+﻿function parseCount(result){
+  let num = Number.parseFloat(result);
+  if(num !== num){
+    throw new Error ("Невалидное значение");
+  }
+  return num;
+}
+
+function validateCount(result){
+  try {
+    return parseCount(result);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle{
+  constructor(a, b, c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if(a + b < c || a + c < b || b + c < a){
+      throw new Error ("Треугольник с такими сторонами не существует");
+    }
+  }
+  get perimeter(){
+    return this.a + this.b + this.c;
+  }
+  get area(){
+    const p = 0.5 * (this.a + this.b + this.c);
+    const result = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Math.round(result * 100) / 100;
+  }
+}
+
+function getTriangle(a, b, c){
+  try {
+    return new Triangle (a, b, c);
+  } catch (error) {
+    console.log("Ошибка! Треугольник не существует");
+    return error;
+  }
+} 
