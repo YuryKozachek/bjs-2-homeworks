dc0eb94a77bd23c17c87fff50a5d15233419687a
YuryKozachek <show_@mail.ru>
2023-07-20 11:15:56 +0300

    exception task fix2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,30 +14,38 @@ function validateCount(result){
   }
 }
 
-class Triangle{
-  constructor(a, b, c){
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error ("Треугольник с такими сторонами не существует");
+    }
     this.a = a;
     this.b = b;
     this.c = c;
-    if(a + b < c || a + c < b || b + c < a){
-      throw new Error ("Треугольник с такими сторонами не существует");
-    }
-  }
-  get perimeter(){
+  };
+
+  get perimeter() {
     return this.a + this.b + this.c;
   }
-  get area(){
+
+  get area() {
     const p = 0.5 * (this.a + this.b + this.c);
     const result = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
     return Math.round(result * 1000) / 1000;
   }
 }
 
-function getTriangle(a, b, c){
+function getTriangle(a, b, c) {
   try {
-    return new Triangle (a, b, c);
-  } catch (error) {
-    console.log("Ошибка! Треугольник не существует");
-    return error;
+    return new Triangle(a, b, c);
+  } catch(error) {
+   return {
+    get perimeter() {
+      return "Ошибка! Треугольник не существует";
+    },
+    get area() {
+      return "Ошибка! Треугольник не существует";
+    }
+   };
   }
 } 
